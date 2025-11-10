1572b2399df483e2a9ea42133e53669da84e1b59
nmfirdausw <nmfirdausw@gmail.com>
2025-11-10 18:17:19 +0800

    feat(nvim): enhance editor options with UI and persistence settings
    - Add UI options: cursorline, line numbers, signcolumn, and fillchars
    - Configure window split behavior (splitbelow, splitright)
    - Set up code folding with indent method and level 99
    - Enable persistent undo with 1000 levels
    - Disable swap and backup files for cleaner workspace
    - Set global statusline (laststatus=3)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,3 +5,33 @@ vim.g.mapleader = " "
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+
+-- UI and appearance
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+vim.opt.fillchars:append({ eob = " ", foldopen = "-", foldclose = "+" })
+vim.opt.guifont = { "ComicCodeLigatures Nerd Font", ":h14" }
+vim.opt.laststatus = 3
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+
+-- Window and split behavior
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Code folding
+vim.opt.foldenable = true
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.foldmethod = "indent"
+
+-- File handling and persistence
+vim.opt.backup = false
+vim.opt.confirm = true
+vim.opt.history = 1000
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.undolevels = 1000
+vim.opt.writebackup = false
