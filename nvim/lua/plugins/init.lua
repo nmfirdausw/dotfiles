51afd5dd9015eda29b9d0359c1fb419c41ff82d3
nmfirdausw <nmfirdausw@gmail.com>
2025-11-10 18:03:31 +0800

    feat(nvim): add gitsigns plugin for git integration
    - Add gitsigns.nvim to plugin list
    - Configure comprehensive git hunk navigation and management
    - Set up blame functionality with inline and buffer views
    - Add visual mode support for staging/resetting hunks
    - Include keybindings for toggling git-related highlights
    - Configure custom sign column symbols for git changes

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,6 +2,7 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
   "https://github.com/kevinhwang91/nvim-ufo",
   "https://github.com/kevinhwang91/promise-async",
+  "https://github.com/lewis6991/gitsigns.nvim",
 }, {
   confirm = false,
   load = function(plugin)
