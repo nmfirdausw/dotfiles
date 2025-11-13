bd4296a491ad3aabcb3b03d2ebfb7bf15efa5478
nmfirdausw <nmfirdausw@gmail.com>
2025-11-14 07:37:01 +0800

    feat(nvim): add treesitter configuration with parser list and toggle keymap
    - Define list of parsers to auto-install (lua, kanata)
    - Add <leader>th keymap to toggle syntax highlighting per buffer
    - Implement buffer-specific treesitter start/stop functionality
    - Add user notifications for highlighting state changes

---
+++ nvim/lua/config/treesitter.lua
@@ -0,0 +1,25 @@
+-- List of parsers to automatically install
+local parsers = {
+  "lua",
+  "kanata",
+}
+
+-- Toggle syntax highlighting for the current buffer
+vim.keymap.set("n", "<leader>th", function()
+  local buf = vim.api.nvim_get_current_buf()
+  local lang = vim.treesitter.language.get_lang(vim.bo[buf].filetype)
+
+  if not lang or not pcall(vim.treesitter.language.add, lang) then
+    return
+  end
+
+  if vim.treesitter.highlighter.active[buf] then
+    vim.treesitter.stop(buf)
+    vim.notify("Treesitter highlighting disabled for this buffer")
+  else
+    vim.treesitter.start(buf)
+    vim.notify("Treesitter highlighting enabled for this buffer")
+  end
+end, { desc = "Toggle syntax highlighting" })
+
+return parsers
