82fbd6f13521123def99d79c0b86e71977237aa2
nmfirdausw <nmfirdausw@gmail.com>
2025-11-13 21:38:50 +0800

    feat(nvim): add filetype-specific indent configuration
    - Add indent.lua module to configure indentation settings per filetype
    - Use autocmd to apply indent settings based on file type
    - Configure Lua files to use 2 spaces with expandtab

---
+++ nvim/lua/config/indent.lua
@@ -0,0 +1,17 @@
+-- Filetype configuration: { spaces, expandtab }
+local config = {
+  lua = { 2, true },
+}
+
+for filetype, indent_config in pairs(config) do
+  local spaces, expandtab = indent_config[1], indent_config[2]
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = filetype,
+    callback = function()
+      vim.bo.expandtab = expandtab
+      vim.bo.shiftwidth = spaces
+      vim.bo.softtabstop = spaces
+      vim.bo.tabstop = spaces
+    end,
+  })
+end
