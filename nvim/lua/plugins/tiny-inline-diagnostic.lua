8e7f345ae49e4d71642728f3ae12314b2357eb37
nmfirdausw <nmfirdausw@gmail.com>
2025-11-19 22:04:45 +0800

    feat(nvim): add tiny-inline-diagnostic plugin for inline diagnostics

---
+++ nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,17 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function()
+    vim.schedule(function()
+      vim.cmd.packadd("tiny-inline-diagnostic.nvim")
+      require("tiny-inline-diagnostic").setup({
+        preset = "simple",
+        options = {
+          show_source = { enabled = false },
+          use_icons_from_diagnostic = true,
+          throttle = 0,
+          enable_on_insert = false,
+        },
+      })
+    end)
+  end,
+  once = true,
+})
