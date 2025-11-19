e68ff1a8ddc3ac50854814e61f7fe23f63b47b38
nmfirdausw <nmfirdausw@gmail.com>
2025-11-19 21:17:33 +0800

    feat(nvim): add blink.cmp and nvim-web-devicons to plugin list
    - Add nvim-web-devicons plugin for file type icon support
    - Add blink.cmp plugin with version range constraint (1.x to 2.x)
    - Update nvim-treesitter revision
    - Update lock file with new plugin revisions

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,10 +1,12 @@
 vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+  "https://github.com/nvim-tree/nvim-web-devicons",
   "https://github.com/kevinhwang91/nvim-ufo",
   "https://github.com/kevinhwang91/promise-async",
   "https://github.com/lewis6991/gitsigns.nvim",
   "https://github.com/stevearc/conform.nvim",
   "https://github.com/nvim-mini/mini.ai",
+  { src = "https://github.com/Saghen/blink.cmp", version = vim.version.range("1") },
 }, {
   confirm = false,
   load = function(plugin)
