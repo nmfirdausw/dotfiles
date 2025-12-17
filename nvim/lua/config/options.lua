37d8512948fb4025c55a5a253a070d50e6cfe932
nmfirdausw <nmfirdausw@gmail.com>
2025-12-17 21:37:54 +0800

    feat(nvim): add tab and indentation settings with 4-space width

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
