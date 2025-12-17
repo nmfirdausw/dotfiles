cc21da401840295705ba355fda6227f42afe7509
nmfirdausw <nmfirdausw@gmail.com>
2025-12-17 21:57:20 +0800

    feat(nvim): add clipboard integration, split positioning, and global statusline

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,3 +6,8 @@ vim.opt.cursorlineopt = "number"
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.laststatus = 3
