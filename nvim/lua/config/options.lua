996b3bb4c82881ae985bc51416dba48abd8fd158
nmfirdausw <nmfirdausw@gmail.com>
2025-12-17 22:00:58 +0800

    feat(nvim): reorganize options and add persistent undo with file backup disabled

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,13 +1,21 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 
 vim.opt.clipboard = "unnamedplus"
+vim.opt.confirm = true
+vim.opt.laststatus = 3
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.laststatus = 3
+
+vim.opt.backup = false
+vim.opt.history = 1000
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.undolevels = 1000
+vim.opt.writebackup = false
