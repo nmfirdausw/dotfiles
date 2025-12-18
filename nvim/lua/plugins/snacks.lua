a309633c35663681a76673ce00c3a168e803a465
nmfirdausw <nmfirdausw@gmail.com>
2025-12-18 17:15:33 +0800

    feat(nvim): configure snacks picker with custom layout

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,5 +1,22 @@
 vim.cmd.packadd("snacks.nvim")
-require("snacks").setup({})
+require("snacks").setup({
+  picker = {
+    layout = {
+      preview = "main",
+      layout = {
+        backdrop = false,
+        height = 0.382,
+        row = -2,
+        box = "vertical",
+        border = "none",
+        title = "{title}",
+        { win = "preview" },
+        { win = "input", height = 1 },
+        { win = "list" },
+      },
+    },
+  },
+})
 
 Snacks.keymap.set("n", "<leader>r", function()
   Snacks.picker.recent()
