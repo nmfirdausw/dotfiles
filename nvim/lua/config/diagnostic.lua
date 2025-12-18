ec72570119e01adc81598b68613d8b656a98a8cc
nmfirdausw <nmfirdausw@gmail.com>
2025-12-18 09:41:12 +0800

    feat(nvim): add diagnostic configuration with virtual text and lines

---
+++ nvim/lua/config/diagnostic.lua
@@ -0,0 +1,10 @@
+vim.diagnostic.config({
+  underline = true,
+  update_in_insert = true,
+  severity_sort = true,
+  virtual_text = true,
+  virtual_lines = true,
+  float = {
+    border = "none",
+  },
+})
