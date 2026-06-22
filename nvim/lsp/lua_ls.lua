a60a4f57701b864ce43d5b933b6b4b2004aba9f8
nmfirdausw <nmfirdausw@gmail.com>
2026-06-22 16:14:22 +0800

    refactor(nvim/lsp): simplify lua_ls config
    Remove unused settings and consolidate hints config.

--- nvim/lsp/lua_ls.lua
+++ nvim/lsp/lua_ls.lua
@@ -1,55 +1,21 @@
 ---@type vim.lsp.Config
-
-local root_markers1 = {
-  '.emmyrc.json',
-  '.luarc.json',
-  '.luarc.jsonc',
-}
-local root_markers2 = {
-  '.luacheckrc',
-  '.stylua.toml',
-  'stylua.toml',
-  'selene.toml',
-  'selene.yml',
-}
-
 return {
-  cmd = { "lua-language-server" },
-  filetypes = { "lua" },
-  root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers1, root_markers2, { '.git' } }
-    or vim.list_extend(vim.list_extend(root_markers1, root_markers2), { '.git' }),
-  on_init = function(client)
-    if client.workspace_folders then
-      local path = client.workspace_folders[1].name
-      if
-        path ~= vim.fn.stdpath("config")
-        and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
-      then
-        return
-      end
-    end
-
-    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-      runtime = {
-        version = "LuaJIT",
-        path = {
-          "lua/?.lua",
-          "lua/?/init.lua",
-        },
-      },
-      workspace = {
-        checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME,
-        },
-      },
-    })
-  end,
-
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = {
+    { '.emmyrc.json', '.luarc.json', '.luarc.jsonc' },
+    { '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml' },
+    { '.git' },
+  },
+  ---@type lspconfig.settings.lua_ls
   settings = {
     Lua = {
       codeLens = { enable = true },
-      hint = { enable = true, semicolon = "Disable", arrayIndex = "Disable" },
+      hint = {
+        enable = true,
+        semicolon = "Disable",
+        arrayIndex = "Disable"
+      },
     },
   },
 }
