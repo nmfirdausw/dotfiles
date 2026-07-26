49c2180b91ab09b2e7de9088b6acb49b44ee7d48
nmfirdausw <nmfirdausw@gmail.com>
2026-07-27 05:09:44 +0800

    feat(nvim): add blink.cmp completion
    - Install blink.cmp and blink.lib via vim.pack
    - Default keymap preset with ghost text and auto brackets
    - Enable signature help and re-show it after accepting
    - Cmdline completion with menu and no preselect

---
+++ nvim/lua/completion.lua
@@ -0,0 +1,59 @@
+vim.pack.add({
+  "https://github.com/saghen/blink.lib",
+  "https://github.com/saghen/blink.cmp",
+}, { confirm = false })
+
+local blink_cmp = require("blink.cmp")
+
+blink_cmp.build():pwait()
+
+blink_cmp.setup({
+  keymap = {
+    preset = "default",
+  },
+  completion = {
+    accept = {
+      auto_brackets = { enabled = true },
+    },
+    list = {
+      selection = {
+        preselect = true,
+        auto_insert = true,
+      },
+    },
+    documentation = {
+      auto_show = false,
+      auto_show_delay_ms = 100,
+    },
+    ghost_text = { enabled = true },
+  },
+  signature = {
+    enabled = true,
+    window = { show_documentation = false },
+    trigger = {
+      enabled = true,
+    },
+  },
+  cmdline = {
+    keymap = { preset = "inherit" },
+    completion = {
+      menu = { auto_show = true },
+      list = {
+        selection = {
+          preselect = false,
+          auto_insert = false,
+        },
+      },
+    },
+  },
+})
+
+vim.api.nvim_create_autocmd("CompleteDone", {
+  callback = function()
+    vim.defer_fn(function()
+      if vim.api.nvim_get_mode().mode == "i" then
+        blink_cmp.show_signature()
+      end
+    end, 10)
+  end,
+})
