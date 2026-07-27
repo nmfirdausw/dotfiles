9e80d9c2b1ced2090019afd02b3d796a4f79bcac
nmfirdausw <nmfirdausw@gmail.com>
2026-07-27 17:27:19 +0800

    feat(nvim): add treesitter-based folding
    - Expression folds via vim.treesitter.foldexpr, open by
      default with plain fold text

---
+++ nvim/lua/fold.lua
@@ -0,0 +1,7 @@
+vim.opt.foldenable = true
+vim.opt.foldlevel = 99
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = ""
+vim.opt.foldcolumn = "0"
+vim.opt.fillchars:append({fold = " "})
