f8e8cafb0b52aadcf3b4b6377b0777763e046124
nmfirdausw <nmfirdausw@gmail.com>
2026-07-27 15:14:10 +0800

    feat(nvim): add editing keymaps
    - Paste over selection and delete without clobbering registers
    - Shift+up/down moves lines in normal, visual, and insert mode
    - Keep cursor position on join, center screen on search jumps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -9,3 +9,31 @@ vim.keymap.set("n", "<Esc>", function()
   vim.diagnostic.config({ virtual_text = { current_line = true } })
   vim.cmd("nohlsearch")
 end, { desc = "Close floating windows and clear search highlight" })
+
+-- Paste over selection without losing yanked text
+vim.keymap.set("x", "p", [["_dP]], { desc = "Paste" })
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yanking" })
+
+-- Normal Mode: Move current line up or down
+vim.keymap.set("n", "<S-Down>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<S-Up>", ":m .-2<CR>==", { desc = "Move line up" })
+
+-- Visual Mode: Move selected block up or down (maintains selection and re-indents)
+vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Insert Mode: Move current line up or down while typing
+vim.keymap.set("i", "<S-Down>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down (Insert)" })
+vim.keymap.set("i", "<S-Up>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up (Insert)" })
+
+-- Keep selection during indent and unindent
+vim.keymap.set("v", "<", "<gv", { desc = "Unindent" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent" })
+
+-- Join lines without moving cursor
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines"})
+
+-- Auto center cursor after jump to next or previous search result
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result" })
