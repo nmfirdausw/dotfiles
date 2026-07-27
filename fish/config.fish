f87866fbec9794c4061677e7fdf1da7185644b73
nmfirdausw <nmfirdausw@gmail.com>
2026-07-27 19:34:29 +0800

    feat(fish): rebind completion keys
    - Unbind ctrl-i and ctrl-m, freed for nvim buffer navigation
    - ctrl-n opens the completion pager and cycles candidates
    - ctrl-e opens the searchable completion pager

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
 if status is-interactive
     set -g fish_greeting ""
 
+    bind ctrl-i ""
+    bind ctrl-m ""
+    bind ctrl-n complete
+    bind ctrl-e complete-and-search
+
     starship init fish | source
 end
