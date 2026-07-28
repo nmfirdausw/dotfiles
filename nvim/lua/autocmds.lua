f8a4a190a15424d71ebed52dd5d5a9f7941ac4c0
nmfirdausw <nmfirdausw@gmail.com>
2026-07-28 16:08:56 +0800

    feat(nvim): auto-toggle quickfix list around grep
    - open quickfix after :grep/:vimgrep/:grepadd
    - close it on mode change when not focused in the quickfix window

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -4,3 +4,20 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.hl.hl_op()
  end,
 })
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+  desc = "Close quickfix when mode changed",
+  callback = function()
+    if vim.bo.buftype ~= "quickfix" then
+      vim.cmd("cclose")
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+  desc = "Open the quickfix list after grep",
+  pattern = { "grep", "vimgrep", "grepadd" },
+  callback = function()
+    vim.cmd("copen")
+  end,
+})
