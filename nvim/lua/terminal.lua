6c130095bd1c2817501fc09dd96a6f68f4546396
nmfirdausw <nmfirdausw@gmail.com>
2026-07-28 16:36:44 +0800

    feat(nvim): configure terminal shell, scrollback, and exit keymap
    - use fish as :terminal's shell
    - raise scrollback to 1,000,000 lines
    - enable recursive path search (gf, :find)
    - exit terminal mode with Esc Esc

--- nvim/lua/terminal.lua
+++ nvim/lua/terminal.lua
@@ -2,8 +2,14 @@ vim.pack.add({ "https://github.com/willothy/flatten.nvim" }, { confirm = false }
 
 require("flatten").setup({})
 
+vim.opt.shell = "/opt/homebrew/bin/fish"
+vim.opt.scrollback = 1000000
+vim.opt.path = ".,**"
+
 vim.api.nvim_create_autocmd("TermOpen", {
   callback = function()
     vim.cmd("startinsert")
   end,
 })
+
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
