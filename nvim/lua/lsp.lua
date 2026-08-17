ad2e4c67f4d9cba1110c547b0265f12d26a4a8ee
nmfirdausw <nmfirdausw@gmail.com>
2026-08-17 23:03:50 +0800

    feat(nvim): add keymap to toggle codelens
    - <leader>lc mirrors the existing <leader>lh inlay hint toggle.
    - enable intelephense lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,7 +1,12 @@
 vim.lsp.enable({
   "lua_ls",
+  "intelephense",
 })
 
 vim.keymap.set("n", "<leader>lh", function()
   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end, { desc = "" })
+
+vim.keymap.set("n", "<leader>lc", function()
+  vim.lsp.codelens.enable(not vim.lsp.codelens.is_enabled())
+end, { desc = "Toggle codelens" })
