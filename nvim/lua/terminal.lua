7172b470b55ebc262caae966afff4743b1ae9542
nmfirdausw <nmfirdausw@gmail.com>
2026-09-02 22:30:51 +0800

    fix(nvim): scope terminal autocmds to a named augroup
    - add terminal augroup covering TermOpen, BufEnter/WinEnter, and TermRequest autocmds, preventing duplicates on reload
    - move the terminal-exit keymap below the autocmd block (no behavior change)

--- nvim/lua/terminal.lua
+++ nvim/lua/terminal.lua
@@ -6,13 +6,17 @@ vim.opt.shell = "/opt/homebrew/bin/fish"
 vim.opt.scrollback = 1000000
 vim.opt.path = ".,**"
 
+local group = vim.api.nvim_create_augroup("terminal", { clear = true })
+
 vim.api.nvim_create_autocmd("TermOpen", {
+  group = group,
   callback = function()
     vim.cmd("startinsert")
   end,
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
+  group = group,
   callback = function()
     if vim.bo.buftype == "terminal" then
       vim.cmd("startinsert")
@@ -20,9 +24,8 @@ vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
   end,
 })
 
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
 vim.api.nvim_create_autocmd("TermRequest", {
+  group = group,
   callback = function(args)
     local seq = args.data.sequence
     local dir = seq and seq:match("^\027]7;file://[^/]*(.+)")
@@ -31,3 +34,5 @@ vim.api.nvim_create_autocmd("TermRequest", {
     end
   end,
 })
+
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
