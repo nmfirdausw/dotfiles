4c77d0990397d028a791e77f719db931382f2a4c
nmfirdausw <nmfirdausw@gmail.com>
2026-09-02 21:40:57 +0800

    refactor(nvim): drop grep-driven quickfix autocmds, group yank highlight
    - remove ModeChanged and QuickFixCmdPost autocmds that auto-toggled quickfix around grep (grep keymaps were already dropped from keymaps.lua)
    - scope the yank-highlight autocmd into a named augroup to avoid duplicates on reload

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,23 +1,8 @@
+-- Briefly highlight the yanked region
 vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("yank_highlight", { clear = true }),
   desc = "Highlight when yanking text",
   callback = function()
     vim.hl.hl_op()
  end,
-})
-
-vim.api.nvim_create_autocmd("ModeChanged", {
-  desc = "Close quickfix when mode changed",
-  callback = function()
-    if vim.bo.buftype ~= "quickfix" then
-      vim.cmd("cclose")
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd("QuickFixCmdPost", {
-  desc = "Open the quickfix list after grep",
-  pattern = { "grep", "vimgrep", "grepadd" },
-  callback = function()
-    vim.cmd("copen")
-  end,
 })
