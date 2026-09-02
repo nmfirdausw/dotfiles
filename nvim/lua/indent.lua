e3937b924b75fa2f47d2c0e13a27876974acdd25
nmfirdausw <nmfirdausw@gmail.com>
2026-09-02 18:30:19 +0800

    docs(nvim): document indent.lua sections
    - explain default indent settings (4 spaces, no tabs)
    - explain per-filetype override table
    - explain autocmd that applies overrides on FileType

--- nvim/lua/indent.lua
+++ nvim/lua/indent.lua
@@ -1,13 +1,16 @@
+-- Default indent: 4 spaces, no tabs
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
+-- Per-filetype indent overrides
 local overrides = {
   lua = { tabstop = 2, shiftwidth = 2, softtabstop = 2 },
 }
 
+-- Apply the overrides per buffer on FileType
 for ft, opts in pairs(overrides) do
   vim.api.nvim_create_autocmd("FileType", {
     pattern = ft,
